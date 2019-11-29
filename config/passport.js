cd3d4e2af5248c2443fcc0cf243fbbcb5e3a61fc
gdoza5 <gdoza5@gmail.com>
2019-11-29 14:06:32 -0600

    finished serialize and deserialize

--- config/passport.js
+++ config/passport.js
@@ -33,3 +33,12 @@ passport.use(new GoogleStrategy({
 }));
 
 
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user){
+    done(err, user);
+  })
+})
